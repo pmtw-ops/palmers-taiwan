cd966343da2329e7728dbe23505cef9131a215e1
Shuwei Lin <owen10253@gmail.com>
2020-07-16 07:40:38 +0800

    Dev, Change Nav font colors.

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { EXAMPLE_PATH } from '@/lib/constants'
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-pmbrown text-gray-200 px-6 border-b-2 border-orange-500">
+    <footer className="w-full bg-pmbrown-800 text-gray-200 px-6 border-b-2 border-orange-500">
       <div className="container mx-auto max-w-4xl py-6 flex flex-wrap md:flex-no-wrap justify-between items-center text-sm">
         &copy; 2020 E.T. Browne Drug Co., Inc. All Rights Reserved.
 				<div className="pt-4 md:p-0 text-center md:text-right text-xs">
