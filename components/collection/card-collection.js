47bfcb8349bd82d2ad95f85456950561366db1f7
Owen Lin <owen10253@gmail.com>
2020-09-12 11:44:24 +0800

    Dev, collections image display issue fix.

--- components/collection/card-collection.js
+++ components/collection/card-collection.js
@@ -1,14 +1,13 @@
 import Link from 'next/link'
-
-const NEXT_PUBLIC_STRAPI_API_URL = 'http://localhost:1337'
+import { CMS_URL } from '@/lib/constants'
 
 export default function CardCollection({ i, collection, children }) {
   let image = collection.images ? collection.images[0] : undefined
-  let imgUrl = image ? NEXT_PUBLIC_STRAPI_API_URL + image.formats.large.url : "https://placehold.it/1000x550"
+  let imgUrl = image ? CMS_URL + image.formats.medium.url : "https://placehold.it/1000x550"
   let imgAlt = image ? image.alternativeText : "temp"
 
   return (
-    <div className="block m-4 bg-white shadow-sm md:flex">
+    <div className="block m-4 bg-white shadow-sm md:flex" key={i}>
       <div className={i % 2 === 0 ? "" : "md:hidden"}>
         <img className="h-full w-full object-cover" src={imgUrl} alt=""></img>
       </div>
--- pages/contact-us.js
+++ pages/contact-us.js
@@ -60,19 +60,19 @@ export default function Index({ categories, collections, product, allPosts, prev
   )
 }
 
-export async function getStaticProps({ preview = null }) {
-  //const allPosts = (await getAllPostsForHome(preview)) || []
-  // console.log(allPosts)
+// export async function getStaticProps({ preview = null }) {
+//   //const allPosts = (await getAllPostsForHome(preview)) || []
+//   // console.log(allPosts)
 
-  const product = {
-    title: '帕瑪氏水嫩潔顏卸妝乳150g',
-    image: 'https://images.unsplash.com/photo-1493925410384-84f842e616fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    shortDesc: '清潔肌膚，使肌膚淨白柔嫩',
-    price: 1300,
-    rating: 3.5
-  }
+//   const product = {
+//     title: '帕瑪氏水嫩潔顏卸妝乳150g',
+//     image: 'https://images.unsplash.com/photo-1493925410384-84f842e616fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+//     shortDesc: '清潔肌膚，使肌膚淨白柔嫩',
+//     price: 1300,
+//     rating: 3.5
+//   }
 
-  return {
-    props: { product } //allPosts, preview },
-  }
-}
+//   return {
+//     props: { product } //allPosts, preview },
+//   }
+// }
