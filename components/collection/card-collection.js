f07c2857edb5e15245aefa132b87004df5ca6e22
Owen Lin <owen10253@gmail.com>
2020-09-23 21:59:07 +0800

    Dev, Center collection cards.

--- components/collection/card-collection.js
+++ components/collection/card-collection.js
@@ -7,20 +7,20 @@ export default function CardCollection({ i, collection }) {
   let image = collection.images ? collection.images[0] : undefined
   let imgUrl = image ? CMS_URL + image.formats.medium.url.replace("medium_", "") : "https://placehold.it/1000x550"
   let imgAlt = image ? image.alternativeText : "temp"
-  
+  let collectionPath = '/collections/' + collection.name
   return (
-    <div className="block m-4 bg-white shadow-md md:flex" key={i}>
+    <div className="block m-4 bg-white shadow-md items-center md:flex" key={i}>
       <div className={i % 2 === 0 ? "" : "md:hidden"}>
-        <img className="h-full w-full object-cover" src={imgUrl} alt=""></img>
+        <Link href={collectionPath} passHref><img className="h-full w-full object-cover" src={imgUrl} alt={imgAlt}></img></Link>
       </div>
 
       <div className="block w-full text-center">
-        <h1 className="text-pmbrown-600 p-4 text-6xl">{collection.name}</h1>
-        <button className="bg-pmbrown-300 text-gray-100 rounded-md p-2 m-2 hover:bg-pmbrown-100 lg:mt-10">了解更多</button>
+        <Link href={collectionPath} passHref><h1 className="text-pmbrown-600 p-4 text-6xl">{collection.name}</h1></Link>
+        <Link href={collectionPath} passHref><button className="bg-pmbrown-300 text-gray-100 rounded-md p-2 m-2 hover:bg-pmbrown-100 lg:mt-10">了解更多</button></Link>
       </div>
 
       <div className={i % 2 === 0 ? "hidden" : "hidden md:block"}>
-        <img className="h-full w-full object-cover" src={imgUrl} alt=""></img>
+        <Link href={collectionPath} passHref><img className="h-full w-full object-cover" src={imgUrl} alt=""></img></Link>
       </div>
     </div>
   )
