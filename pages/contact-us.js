9d489ba943788eb26b89d5471589514ad4ca41a3
Owen Lin <owen10253@gmail.com>
2020-10-11 22:43:57 -0700

    Dev, update AboutUs and ContactUS images.

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,7 +1,7 @@
 
-import Layout from '@/components/layout'
+import { getContactUs } from '@/lib/api_contact-us'
+
 import Container from '@/components/container'
-import { getAllPostsForHome } from '@/lib/api_products'
 import Head from 'next/head'
 import { CMS_NAME } from '@/lib/constants'
 
@@ -9,13 +9,20 @@ import CardProduct from '@/components/product/card-product'
 
 import Link from 'next/link'
 
+const CMS_URL = process.env.NEXT_PUBLIC_USE_DEV_DB === 'true' ? process.env.NEXT_PUBLIC_DEV_API_URL : process.env.NEXT_PUBLIC_PROD_API_URL;
 
-export default function Index({ categories, collections, product, allPosts, preview }) {
-  // const heroPost = allPosts[0]
-  // const morePosts = allPosts.slice(1)
+export default function Index({ contactUs }) {
+  let image = contactUs?.image[0];
+  let imgName = image?.name;
+  let imgAlt = image?.alternativeText;
+  let imgCaption = image?.caption;
+  let imgUrl = image ? CMS_URL + image.formats.medium.url.replace("medium_", "") : "https://placehold.it/1000x650"
+
   return (
     <>
-      <img className="" src="https://placehold.it/1920x550" alt=""></img>
+      <div>
+        <img src={imgUrl} alt={imgAlt}></img>
+      </div>
       <Container>
         <h1 className="p-2 mt-5 font-bold text-pmbrown-800 text-6xl text-center">需要協助？</h1>
         <h1 className="p-2 font-bold text-pmbrown-700 text-3xl text-center">請留下您的聯絡訊息</h1>
@@ -23,12 +30,12 @@ export default function Index({ categories, collections, product, allPosts, prev
           <div className="px-4">
             <h3 className="p-2 font-bold text-pmbrown-800 text-2xl">我們的資料</h3>
             <h5 className="p-2 mt-4 text-pmbrown-800 text-xl">客服信箱：</h5>
-            <div className="p-2 break-words text-pmbrown-600">emailAddress@PalmersTaiwan.com.tw</div>
+            <div className="p-2 break-words text-pmbrown-600">{contactUs.contactEmail}</div>
             <h5 className="p-2 mt-4 text-pmbrown-800 text-xl">客服電話：</h5>
-            <div className="p-2 text-pmbrown-600">0800-555-988</div>
+            <div className="p-2 text-pmbrown-600">{contactUs.contactPhone}</div>
             <div className="p-2 text-pmbrown-600">(星期一～星期五 10:00~12:00/14:00~17:00，國定假日除外)</div>
             <h5 className="p-2 mt-4 text-pmbrown-800 text-xl">地址：</h5>
-            <div className="p-2 text-pmbrown-600">台中市梧棲區自立路373號</div>
+            <div className="p-2 text-pmbrown-600">{contactUs.address}</div>
           </div>
           <div>
             <form className="w-full max-w-sm p-4">
@@ -49,7 +56,7 @@ export default function Index({ categories, collections, product, allPosts, prev
               <div className="mt-4">
                 <span className="inline-flex rounded-md shadow-sm">
                   <button type="button" className="inline-flex px-4 py-2 border border-gray-300 text-base leading-6 font-medium rounded-md text-pmbrown-700 bg-white hover:text-pmbrown-200 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150">
-                  送出</button>
+                    送出</button>
                 </span>
               </div>
             </form>
@@ -60,19 +67,10 @@ export default function Index({ categories, collections, product, allPosts, prev
   )
 }
 
-// export async function getStaticProps({ preview = null }) {
-//   //const allPosts = (await getAllPostsForHome(preview)) || []
-//   // console.log(allPosts)
+export async function getStaticProps({ preview = null }) {
+  const contactUs = (await getContactUs()) || []
 
-//   const product = {
-//     title: '帕瑪氏水嫩潔顏卸妝乳150g',
-//     image: 'https://images.unsplash.com/photo-1493925410384-84f842e616fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-//     shortDesc: '清潔肌膚，使肌膚淨白柔嫩',
-//     price: 1300,
-//     rating: 3.5
-//   }
-
-//   return {
-//     props: { product } //allPosts, preview },
-//   }
-// }
+  return {
+    props: { contactUs }
+  }
+}
