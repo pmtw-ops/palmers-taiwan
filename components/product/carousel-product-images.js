1c06f22f5ad461bfe62c9faab5a9053b93282894
Owen Lin <owen10253@gmail.com>
2020-12-05 11:55:29 -0800

    Dev, update product image carousel.

--- components/product/carousel-product-images.js
+++ components/product/carousel-product-images.js
@@ -3,7 +3,6 @@ import Link from 'next/link'
 const CMS_URL = process.env.NEXT_PUBLIC_USE_DEV_DB === 'true' ? process.env.NEXT_PUBLIC_DEV_API_URL : process.env.NEXT_PUBLIC_PROD_API_URL;
 
 export default function CarouselProductImages({ images }) {
-  console.log(images)
   return (
     <div className="block md:p-0 md:flex md:justify-around">
       <div className="hidden md:inline-grid md:gap-2 md:p-2 md:grid-col-1">
@@ -28,8 +27,24 @@ export default function CarouselProductImages({ images }) {
         <img className="p-2 object-cover" src="https://placehold.it/600" alt=""></img>
       </div>
 
-      <div className="md:hidden">
-        Row image selector
+      <div className="mt-4 md:hidden">
+        <div className="flex justify-around">
+          <div className="">
+            <img className="border" src="https://placehold.it/80/FFFFFF/FFFFFF"></img>
+          </div>
+          <div className="">
+            <img className="border" src="https://placehold.it/80/FFFFFF/FFFFFF"></img>
+          </div>
+          <div className="">
+            <img className="border" src="https://placehold.it/80/FFFFFF/FFFFFF"></img>
+          </div>
+          <div className="">
+            <img className="border" src="https://placehold.it/80/FFFFFF/FFFFFF"></img>
+          </div>
+          <div className="">
+            <img className="border" src="https://placehold.it/80/FFFFFF/FFFFFF"></img>
+          </div>
+        </div>
       </div>
     </div >
   )
