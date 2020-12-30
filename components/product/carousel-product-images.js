4ca0ebf0ce830ec1323057afa4f7361b9efcf680
Owen Lin <owen10253@gmail.com>
2020-12-29 22:31:03 -0800

    Dev, added featureImage.

--- components/product/carousel-product-images.js
+++ components/product/carousel-product-images.js
@@ -1,8 +1,34 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 const CMS_URL = process.env.NEXT_PUBLIC_USE_DEV_DB === 'true' ? process.env.NEXT_PUBLIC_DEV_API_URL : process.env.NEXT_PUBLIC_PROD_API_URL;
 
 export default function CarouselProductImages({ images }) {
+  var featureImages = [];
+  
+  /* --- Get Images List --- */
+  for (var i = 0; i < images.length; i++) {
+    var thumbnailUrl = CMS_URL + images[i].formats.thumbnail.url;
+    var url = thumbnailUrl.replace('thumbnail_', '');
+    var alt = images[i].alternativeText;
+    var cap = images[i].caption;
+    var image = {
+      url: url,
+      thumbnail: thumbnailUrl,
+      alt: alt,
+      cap: cap
+    }
+    featureImages.push(image);
+  }
+  
+  let [mainImage, setMainImage] = useState(featureImages[0]? featureImages[0].url : "https://via.placeholder.com/600");
+  
+  const renderMainImage = (i) => {
+    setMainImage(featureImages[i].url);
+  }
+  
+  
+  
   return (
     <div className="block lg:p-0 lg:flex lg:justify-around">
       <div className="hidden lg:inline-grid lg:gap-2 lg:p-2 lg:grid-col-1">
@@ -24,7 +50,7 @@ export default function CarouselProductImages({ images }) {
       </div>
 
       <div className="" >
-        <img className="p-2 object-cover" src="https://via.placeholder.com/600" alt=""></img>
+        <img className="p-2 object-cover" src={mainImage} alt=""></img>
       </div>
 
       <div className="mt-2 mb-2 lg:hidden">
