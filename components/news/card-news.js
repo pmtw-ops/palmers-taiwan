d1ad98320311ddb205b06d724f15cf4644781933
Owen Lin <owen10253@gmail.com>
2021-01-08 14:47:49 -0800

    Dev, update News Post card design.

--- components/news/card-news.js
+++ components/news/card-news.js
@@ -5,12 +5,14 @@ const CMS_URL = process.env.NEXT_PUBLIC_USE_DEV_DB === 'true' ? process.env.NEXT
 export default function CardNews() {
   return (
     <div className="w-full sm:w-1/2 md:w-1/2 my-3 px-3">
-      <div className="relative shadow-lg bg-white">
-        <picture className="block cursor-pointer bg-gray-200">
-          <img className="block" src="https://via.placeholder.com/550x250" alt=""></img>
-        </picture>
-        <div className="p-4 mt-2 text-center">
-          This is the title of the news post.
+      <div className="shadow-md bg-white">
+        <div className="relative" style={{ paddingBottom: "50%" }}>
+          <img className="absolute cursor-pointer object-cover" src={"https://via.placeholder.com/650x325"} alt=""></img>
+        </div>
+        <div className="p-4">
+          <div>
+            This is the title of the news post.
+          </div>
         </div>
       </div>
     </div>
