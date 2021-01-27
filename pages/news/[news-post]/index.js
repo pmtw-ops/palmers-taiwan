2236436a34cc2d97bea8ea20c3a884127e5ea7d7
Owen Lin <owen10253@gmail.com>
2021-01-26 21:40:02 -0800

    Dev, news-post placeholder.

---
+++ pages/news/[news-post]/index.js
@@ -0,0 +1,44 @@
+
+import Layout from '@/components/layout'
+import Container from '@/components/container'
+import { getAllPostsForHome } from '@/lib/api_products'
+import NewsPost from '@/components/news/news-post'
+import Head from 'next/head'
+import { CMS_NAME } from '@/lib/constants'
+
+import CardProduct from '@/components/product/card-product'
+
+import Link from 'next/link'
+
+const NEXT_PUBLIC_STRAPI_API_URL = process.env.NEXT_PUBLIC_STRAPI_API_URL
+
+
+export default function Index({ categories, collections, product, allPosts, preview }) {
+  // const heroPost = allPosts[0]
+  // const morePosts = allPosts.slice(1)
+  return (
+    <>
+      <img className="" src="https://via.placeholder.com/1920x550" alt=""></img>
+      <Container>
+     
+      </Container>
+    </>
+  )
+}
+
+export async function getStaticProps({ preview = null }) {
+  //const allPosts = (await getAllPostsForHome(preview)) || []
+  // console.log(allPosts)
+
+  const product = {
+    title: '帕瑪氏水嫩潔顏卸妝乳150g',
+    image: 'https://images.unsplash.com/photo-1493925410384-84f842e616fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    shortDesc: '清潔肌膚，使肌膚淨白柔嫩',
+    price: 1300,
+    rating: 3.5
+  }
+
+  return {
+    props: { product } //allPosts, preview },
+  }
+}
