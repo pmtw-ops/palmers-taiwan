0b699b728e069a41a9052ed8a786a00e55df8b2f
Owen Lin <owen10253@gmail.com>
2021-02-16 21:48:51 -0800

    Dev, change container size. udpate About images.

--- components/container.js
+++ components/container.js
@@ -1,3 +1,3 @@
 export default function Container({ children }) {
-  return <div className="container mx-auto px-2">{children}</div>
+  return <div className="container max-w-screen-xl px-2">{children}</div>
 }
